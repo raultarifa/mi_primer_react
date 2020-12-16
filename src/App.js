7116d9d489979d5b66cef8fb2ad51b9dbf2154ae
Raúl Muñoz Ortega <raul@Air-de-Raul.home>
2020-12-16 14:03:24 +0100

    Mi primer componente

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Contador from './components/Contador.js'
 
 function App() {
   return (
-    <h1>Hola Mundo</h1>
+    <div>
+      <h1>Hola Mundo</h1>
+      <Contador />
+    </div>
   );
 }
 
---
+++ src/components/Contador.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Contador = () => {
+    return (
+        <h3>Mi primer componente</h3>
+      );
+}
+ 
+export default Contador;
