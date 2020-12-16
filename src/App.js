b5a2b2a432aa309c15570201d51ebef8ea208ece
Raúl Muñoz Ortega <raul@Air-de-Raul.home>
2020-12-16 18:54:10 +0100

    Formulario

--- src/App.js
+++ src/App.js
@@ -2,18 +2,24 @@ import React from 'react';
 import Contador from './components/Contador.js'
 import Jsx from './components/Jsx.js'
 import Lista from './components/Lista.js'
+import Formulario from './components/Formulario.js'
 
 
 function App() {
 
-const saludo ='Hola Jsx'
+const saludo ='Prueba'
 
   return (
-    <div>
-      <h1>Hola Mundo {saludo}</h1>
+    <div className="container mt-5">
+      <h1>Formulario de {saludo}</h1>
+      {/* Un comentario JSX */}
+      {/*
+      
       <Contador />
       <Jsx />
       <Lista />
+      */}
+      <Formulario />
     </div>
   );
 }
---
+++ src/components/Formulario.js
@@ -0,0 +1,66 @@
+
+import React ,{Fragment, useState}from 'react';
+
+
+
+const Formulario = () => {
+
+    const[datos,setDatos]=useState({
+        nombre:'',
+        apellidos:''
+    })
+
+    const escribirinput = (event) =>{
+        {/*console.log(event.target.value)*/}
+        setDatos({
+            ...datos,
+           [event.target.name] : event.target.value,      
+        })        
+    }
+
+    const enviarDatos = (event) =>{
+       {/* evitamos el procesamiento automatico con la siguiente funcion */} 
+       event.preventDefault();
+       console.log(datos.nombre + ' ' + datos.apellidos)
+
+
+
+
+    }
+
+    return ( 
+        <Fragment>
+            <h5>Componente</h5>
+            <form className="row" onSubmit={enviarDatos}>
+                
+                <div className="col-md-3">
+                    <input 
+                        placeholder="Ingrese nombre" 
+                        className="form-control mb-3"
+                        type="text"
+                        name="nombre"
+                        onChange={escribirinput}>
+                    </input>
+                </div>
+                
+                <div className="col-md-3">
+                    <input 
+                        placeholder="Ingrese apellidos" 
+                        className="form-control mb-3"
+                        type="text"
+                        name="apellidos"
+                        onChange={escribirinput}>
+                    </input>
+                </div>
+                
+                <div className="col-md-3">
+                    <button className="btn btn-primary" type="submit">Enviar</button>
+                </div>
+            </form>
+            <h4>{datos.nombre} - {datos.apellidos}</h4>
+       </Fragment>
+        
+     );
+}
+ 
+export default Formulario;
